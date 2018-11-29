c623722349d4431a5d2f4dedc4247c1f059ce2d9
vshevch <vlad.shev@gmail.com>
2018-11-28 22:24:02 -0500

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+// Enter into Root so that I don't have to use SUDO
+sudo bash 
+apt-get install apache2 apache2-doc apache2-utils 
+// Might be asked if you would like to continue
+Y
